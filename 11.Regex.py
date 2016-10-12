ce732e1b1fd006d00521df418fbc7b952eddf722
ChongDen <dchong@gmail.com>
2016-10-12 09:07:47 -0400

    initial commit for regex

--- 11.Regex.py
+++ 11.Regex.py
@@ -5,10 +5,11 @@
 # ^ --> starts with
 # . --> matches character
 # * --> any number of times (everything)
-# \S --> any non-blank character
-
+# \S --> any non-whitespace character
+# .+ --> greedy matching
+# .+? --> non-greedy matching
 import re
-
+'''
 #re.search() returns T/F
 file = open('test.txt')
 for line in file:
@@ -20,4 +21,15 @@ for line in file:
 x = "2 + 1 = 3...lalalalal"
 y = re.findall('[0-9]+', x) #find 0-9 number or more
 print(y)
+'''
+#greedy matching
+#If you have a longer string, take the longer.
+with open('test.txt', 'r') as myFile:
+    data = myFile.read()
+y = re.findall('^From (\S+@\S+)', data, re.MULTILINE)
+#print(y)
+
+x = re.findall('^From .*@([^ ]*)', data, re.MULTILINE)
+print(x)
+
 
